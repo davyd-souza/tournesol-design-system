52c7c2a091be851b98c270d3958691372eee92be
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-05-13 03:00:40 -0300

    chore(react): create example button for storybook

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from './styles'
+
+export const Button = styled('button', {
+  fontFamily: '$sans',
+  backgroundColor: '$yellow400',
+  borderRadius: '$md',
+  paddingInline: '$4',
+  paddingBlock: '$2',
+})
