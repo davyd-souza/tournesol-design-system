fc11bf418fc3368aeaec4f562bf29f013aeaacec
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-05-31 00:42:04 -0300

    feat(docs): implement arg types for text story

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -7,6 +7,36 @@ export default {
   component: Text,
   args: {
     children: 'Big ball of wibbly wobbly, timey wimey stuff',
+    size: 'base',
+  },
+  argTypes: {
+    size: {
+      name: 'size',
+      description: 'Control text size',
+      control: 'select',
+      table: {
+        type: {
+          summary:
+            '"xs" | "sm" | "base" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl" | "6xl" | "7xl" | "8xl" | "9xl"',
+        },
+        defaultValue: { summary: '"base"' },
+      },
+      options: [
+        'xs',
+        'sm',
+        'base',
+        'lg',
+        'xl',
+        '2xl',
+        '3xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+    },
   },
 } as Meta<TextProps>
 
