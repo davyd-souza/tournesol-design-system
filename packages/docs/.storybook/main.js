5b37b7be20471fd3de77ebd89134244d8308784a
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-06-02 23:28:01 -0300

    build: update storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,28 +1,21 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
-  },
-  core: {
-    builder: '@storybook/builder-vite'
+    options: {}
   },
   docs: {
-    autodocs: true,
+    autodocs: true
   },
-  viteFinal: (config, { configType }) => {
+  viteFinal: (config, {
+    configType
+  }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/tournesol-design-system/'
+      config.base = '/tournesol-design-system/';
     }
-
-    return config
+    return config;
   }
 };
 export default config;
